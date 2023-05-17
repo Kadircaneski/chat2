afc95f4ba6bfedde350092b503246c613557440b
Kadircan Eski <kadircaneski@hotmail.com>
2023-05-17 19:36:06 +0300

    registration & login

---
+++ lib/screens/splash.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SlapshScreen extends StatelessWidget {
+  const SlapshScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('esChat'),
+      ),
+      body: const Center(
+        child: Text('Loading...'),
+      ),
+    );
+  }
+}
